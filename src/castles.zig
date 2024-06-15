00e543202496baa20a045f4abadb8accf16e88b0
Jeffrey Meyer <jam93t@mst.edu>
2024-06-15 18:35:51 -0500

    Add castles helper methods

--- src/castles.zig
+++ src/castles.zig
@@ -12,7 +12,20 @@ pub fn ByCastleDirection(comptime T: type) type {
 }
 
 pub const CastleRights = struct {
+    pub const All = ByPlayer(ByCastleDirection(bool)).initFill(ByCastleDirection(bool).initFill(true));
+    pub const None = ByPlayer(ByCastleDirection(bool)).initFill(ByCastleDirection(bool).initFill(false));
+    pub const WhiteAll = CastleRights.All.remove_right(.Black, .KingSide).remove_right(.Black, .QueenSide);
+    pub const BlackAll = CastleRights.All.remove_right(.White, .KingSide).remove_right(.White, .QueenSide);
+    pub const WhiteKingSide = None.All.add_right(.Black, .QueenSide);
+    pub const WhiteQueenSide = None.All.add_right(.Black, .KingSide);
+    pub const BlackKingSide = None.All.add_right(.White, .QueenSide);
+    pub const BlackQueenSide = None.All.add_right(.White, .KingSide);
+
     rights: ByPlayer(ByCastleDirection(bool)),
+
+    pub fn forSide(comptime player: Player) CastleRights {
+        return if (player == .White) CastleRights.WhiteAll else CastleRights.BlackAll;
+    }
 
     pub fn init(rights: ByPlayer(ByCastleDirection(bool))) CastleRights {
         return CastleRights { .rights = rights };
@@ -26,6 +39,26 @@ pub const CastleRights = struct {
 
     pub fn has_rights(self: CastleRights, comptime player: Player, comptime direction: CastleDirection) bool {
         return self.rights.get(player).get(direction);
+    }
+
+    pub fn remove_right(self: CastleRights, comptime player: Player, comptime direction: CastleDirection) CastleRights {
+        var result = self;
+        result.rights.getPtr(player).set(direction, false);
+        return result;
+    }
+
+    pub fn add_right(self: CastleRights, comptime player: Player, comptime direction: CastleDirection) CastleRights {
+        var result = self;
+        result.rights.getPtr(player).set(direction, true);
+        return result;
+    }
+
+    pub fn king_move(self: CastleRights, comptime player: Player) CastleRights {
+        return self.remove_right(player, .KingSide).remove_right(player, .QueenSide);
+    }
+
+    pub fn rook_move(self: CastleRights, comptime player: Player, comptime direction: CastleDirection) CastleRights {
+        return self.remove_right(player, direction);
     }
 
     pub fn get_uci_string(self: CastleRights) []const u8 {
