25c2c62cb4a42874684348a8d4c825f95832139b
Jeffrey Meyer <jmeyer@swankmp.com>
2024-06-23 16:13:39 -0500

    Add relative and pawn-attack directions

--- src/directions.zig
+++ src/directions.zig
@@ -46,3 +46,64 @@ pub const BoardDirection = enum(i5) {
 pub fn ByBoardDirection(comptime T: type) type {
     return std.EnumArray(BoardDirection, T);
 }
+
+/// A relative direction on the board, relative to a player's perspective.
+pub const RelativeDirection = enum(u3) {
+    forward,
+    backward,
+    left,
+    right,
+    forward_left,
+    forward_right,
+    backward_left,
+    backward_right,
+
+    /// Given a perspective, returns the corresponding `BoardDirection`.
+    pub fn toDirection(self: RelativeDirection, comptime perspective: Player) BoardDirection {
+        return switch (self) {
+            .forward => if (perspective == .white) .north else .south,
+            .backward => if (perspective == .white) .south else .north,
+            .left => if (perspective == .white) .west else .east,
+            .right => if (perspective == .white) .east else .west,
+            .forward_left => if (perspective == .white) .north_west else .north_east,
+            .forward_right => if (perspective == .white) .north_east else .north_west,
+            .backward_left => if (perspective == .white) .south_west else .south_east,
+            .backward_right => if (perspective == .white) .south_east else .south_west,
+        };
+    }
+
+    test toDirection {
+        try std.testing.expectEqual(RelativeDirection.forward.toDirection(.white), BoardDirection.north);
+        try std.testing.expectEqual(RelativeDirection.forward.toDirection(.black), BoardDirection.south);
+        try std.testing.expectEqual(RelativeDirection.backward.toDirection(.white), BoardDirection.south);
+        try std.testing.expectEqual(RelativeDirection.backward.toDirection(.black), BoardDirection.north);
+        try std.testing.expectEqual(RelativeDirection.left.toDirection(.white), BoardDirection.west);
+        try std.testing.expectEqual(RelativeDirection.left.toDirection(.black), BoardDirection.east);
+        try std.testing.expectEqual(RelativeDirection.right.toDirection(.white), BoardDirection.east);
+        try std.testing.expectEqual(RelativeDirection.right.toDirection(.black), BoardDirection.west);
+        try std.testing.expectEqual(RelativeDirection.forward_left.toDirection(.white), BoardDirection.north_west);
+        try std.testing.expectEqual(RelativeDirection.forward_left.toDirection(.black), BoardDirection.north_east);
+        try std.testing.expectEqual(RelativeDirection.forward_right.toDirection(.white), BoardDirection.north_east);
+        try std.testing.expectEqual(RelativeDirection.forward_right.toDirection(.black), BoardDirection.north_west);
+        try std.testing.expectEqual(RelativeDirection.backward_left.toDirection(.white), BoardDirection.south_west);
+        try std.testing.expectEqual(RelativeDirection.backward_left.toDirection(.black), BoardDirection.south_east);
+        try std.testing.expectEqual(RelativeDirection.backward_right.toDirection(.white), BoardDirection.south_east);
+        try std.testing.expectEqual(RelativeDirection.backward_right.toDirection(.black), BoardDirection.south_west);
+    }
+};
+
+/// Represents the relative direction a pawn can attack in.
+pub const PawnAttackDirection = enum(u3) {
+    forward_left = @intFromEnum(RelativeDirection.forward_left),
+    forward_right = @intFromEnum(RelativeDirection.forward_right),
+
+    /// Returns the `RelativeDirection` corresponding to this `PawnAttackDirection`.s
+    pub fn toRelativeDirection(self: PawnAttackDirection) RelativeDirection {
+        return @enumFromInt(@intFromEnum(self));
+    }
+
+    test toRelativeDirection {
+        try std.testing.expectEqual(PawnAttackDirection.forward_left.toRelativeDirection(), RelativeDirection.forward_left);
+        try std.testing.expectEqual(PawnAttackDirection.forward_right.toRelativeDirection(), RelativeDirection.forward_right);
+    }
+};
