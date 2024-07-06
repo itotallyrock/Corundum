4b3c2e4c9d1e96b7f9f36064502644661fc8294c
Jeffrey Meyer <jmeyer@swankmp.com>
2024-07-06 02:16:30 -0500

    Beef up build script

--- build.zig
+++ build.zig
@@ -1,61 +1,36 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "corundum",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
+    const exe_name = b.option(
+        []const u8,
+        "exe_name",
+        "Name of the executable",
+    ) orelse "corundum";
+
+    const build_options = b.addOptions();
+    build_options.addOption([]const u8, "version", "0.0.1");
+
+    const exe = b.addExecutable(.{
+        .name = exe_name,
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
+    exe.root_module.addOptions("build_options", build_options);
 
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    b.installArtifact(lib);
+    b.installArtifact(exe);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
+    b.step("run", "Run the application").dependOn(&b.addRunArtifact(exe).step);
+
     const main_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
+    main_tests.root_module.addOptions("build_options", build_options);
 
-    const run_main_tests = b.addRunArtifact(main_tests);
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build test`
-    // This will evaluate the `test` step rather than the default, which is "install".
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&run_main_tests.step);
-
-    const exe = b.addExecutable(.{
-        .name = "corundum",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    b.installArtifact(exe);
-    const run_exe = b.addRunArtifact(exe);
-
-    const run_step = b.step("run", "Run the application");
-    run_step.dependOn(&run_exe.step);
+    b.step("test", "Run tests").dependOn(&b.addRunArtifact(main_tests).step);
 }
