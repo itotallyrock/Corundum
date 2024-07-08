0fa0c517bbe9f04d62fc7935acdaf1f52792af33
Jeffrey Meyer <jmeyer@swankmp.com>
2024-07-08 10:01:04 -0500

    WIP Options parsing

--- src/uci.zig
+++ src/uci.zig
@@ -1,6 +1,11 @@
 const std = @import("std");
 const build_options = @import("build_options");
 
+pub const SetOption = struct {
+    name: []const u8,
+    value: ?[]const u8,
+};
+
 /// A parsed incoming UCI command
 pub const UciCommand = union(enum) {
     uci,
@@ -9,10 +14,7 @@ pub const UciCommand = union(enum) {
     },
     isready,
     ucinewgame,
-    setoption: struct {
-        name: []const u8,
-        value: ?[]const u8,
-    },
+    setoption: SetOption,
     position: struct {
         position: union(enum) {
             fen: []const u8,
@@ -46,41 +48,74 @@ pub const UciCommand = union(enum) {
     },
 
     pub fn tryParse(line: []const u8) !UciCommand {
-        if (std.mem.eql(u8, line, "uci")) {
+        if (std.ascii.eqlIgnoreCase(line, "uci")) {
             return .uci;
-        } else if (std.mem.eql(u8, line, "isready")) {
+        } else if (std.ascii.eqlIgnoreCase(line, "isready")) {
             return .isready;
-        } else if (std.mem.eql(u8, line, "ucinewgame")) {
+        } else if (std.ascii.eqlIgnoreCase(line, "ucinewgame")) {
             return .ucinewgame;
-        } else if (std.mem.eql(u8, line, "ponderhit")) {
+        } else if (std.ascii.eqlIgnoreCase(line, "ponderhit")) {
             return .ponderhit;
-        } else if (std.mem.eql(u8, line, "quit")) {
+        } else if (std.ascii.eqlIgnoreCase(line, "quit")) {
             return .quit;
-        } else if (std.mem.eql(u8, line, "stop")) {
+        } else if (std.ascii.eqlIgnoreCase(line, "stop")) {
             return .stop;
+        } else if (std.ascii.eqlIgnoreCase(line, "debug on")) {
+            return .{ .debug = .{ .on = true } };
+        } else if (std.ascii.eqlIgnoreCase(line, "debug off")) {
+            return .{ .debug = .{ .on = false } };
+        } else if (std.ascii.startsWithIgnoreCase(line, "register ")) {
+            @panic("TODO: Parse register command");
+        } else if (std.ascii.startsWithIgnoreCase(line, "setoption name ")) {
+            const name_and_maybe_value = line[15..];
+            std.debug.print("WOWOWO '{s}' ''\n", .{line});
+            _ = name_and_maybe_value;
+            @panic("TODO");
+            // if (std.mem.lastIndexOf(u8, name_and_maybe_value, " value ")) |value_index| {
+            //     const name = name_and_maybe_value[0..value_index];
+            //     const value = name_and_maybe_value[value_index + 7 ..];
+            //     return .{ .setoption = .{ .name = name, .value = value } };
+            // } else {
+            //     return .{ .setoption = .{ .name = name_and_maybe_value, .value = null } };
+            // }
+        } else if (std.ascii.startsWithIgnoreCase(line, "position ")) {
+            @panic("TODO: Parse position command");
+        } else if (std.ascii.startsWithIgnoreCase(line, "go ")) {
+            @panic("TODO: Parse go command");
         }
         // TODO: The rest
         return error.UnrecognizedCommand;
     }
 };
 
-pub const UciCommandParseError = error{ UnrecognizedCommand, UciNotImplemented };
+pub const UciCommandParseError = error{UnrecognizedCommand};
 
-pub fn parse(command: []const u8) !UciCommand {
-    _ = command;
-    return error.UciNotImplemented;
-}
+pub const UciOptionType = enum {
+    check,
+    spin,
+    combo,
+    string,
+    button,
+};
+
+pub const UciOptionValue = union(UciOptionType) {
+    check: bool,
+    spin: i32,
+    combo: []const u8,
+    string: []const u8,
+    button,
+};
 
 pub const UciOptionConfig = struct {
     name: []const u8,
-    config: union(enum) {
+    config: union(UciOptionType) {
         check: struct {
             default: bool = false,
         },
         spin: struct {
-            default: u32,
-            min: u32,
-            max: u32,
+            default: i32,
+            min: i32,
+            max: i32,
         },
         combo: struct {
             default: []const u8,
@@ -91,6 +126,42 @@ pub const UciOptionConfig = struct {
         },
         button,
     },
+
+    pub fn tryParse(self: UciOptionConfig, value: ?[]const u8) !UciOptionValue {
+        // TODO: Move this to the caller of this method
+        // if (!std.ascii.eqlIgnoreCase(self.name, set_option.name)) {
+        //     return null;
+        // }
+
+        switch (self.config) {
+            .check => {
+                if (value) |v| {
+                    if (std.ascii.eqlIgnoreCase(v, "true")) {
+                        return .{ .check = true };
+                    } else if (std.ascii.eqlIgnoreCase(v, "false")) {
+                        return .{ .check = false };
+                    } else {
+                        return error.InvalidOptionValue;
+                    }
+                } else {
+                    return .{ .check = self.config.check.default };
+                }
+            },
+            .spin => {
+                if (value) |v| {
+                    const i = try std.fmt.parseInt(i32, v, 10);
+                    if (i < self.config.spin.min or i > self.config.spin.max) {
+                        return error.InvalidOptionValue;
+                    }
+                    return .{ .spin = i };
+                } else {
+                    return .{ .spin = self.config.spin.default };
+                }
+            },
+            .button => return .button,
+            else => @panic("TODO: Implement the rest of the option types"),
+        }
+    }
 };
 
 pub const UciResponse = union(enum) {
@@ -147,10 +218,9 @@ pub const CliCommand = union(enum) {
     uci: UciCommand,
 
     pub fn tryParse(line: []const u8) CliCommandParseError!CliCommand {
-        // TODO: Consider lowercasing the input for case-insensitivity
-        if (std.mem.eql(u8, line, "bench")) {
+        if (std.ascii.eqlIgnoreCase(line, "bench")) {
             return .bench;
-        } else if (std.mem.eql(u8, line, "perft")) {
+        } else if (std.ascii.eqlIgnoreCase(line, "perft")) {
             // TODO: Parse FEN and depth
             return .{ .perft = .{ .fen = undefined, .depth = 0 } };
         }
@@ -238,6 +308,33 @@ pub const CliManager = struct {
                             // TODO: Initialize the engine
                             try self.writeResponse(.readyok);
                             self.state = .{ .uci = .initialized };
+                        },
+                        .setoption => |option| {
+                            for (engine_options) |engine_option| {
+                                if (std.ascii.eqlIgnoreCase(option.name, engine_option.name)) {
+                                    const value = engine_option.tryParse(option.value) catch {
+                                        try self.output_stream.print("Invalid value for option '{s}': {s}\n", .{ option.name, option.value orelse "" });
+                                        continue;
+                                    };
+                                    _ = value;
+                                    std.debug.print("Setting option {s}\n", .{option.name});
+                                    // TODO: Update engine with option
+
+                                    continue;
+                                }
+                            }
+                            try self.output_stream.print("Unknown option: '{s}'.\n", .{option.name});
+                        },
+                        .position => |position_and_moves| {
+                            _ = position_and_moves;
+                            // TODO: Implement set position
+                        },
+                        .go => |go_options| {
+                            _ = go_options;
+                            // TODO: Implement go
+                        },
+                        .stop => {
+                            // TODO: Implement stop
                         },
                         // TODO: The rest of the UCI commands
                         .quit => break,
@@ -337,6 +434,8 @@ pub const CliManager = struct {
         var line = std.BoundedArray(u8, base_buffer_size).init(0) catch unreachable;
         try self.input_stream.streamUntilDelimiter(line.writer(), '\n', base_buffer_size);
         const trimmed = std.mem.trim(u8, line.slice(), &std.ascii.whitespace);
+
+        std.debug.print("Read line '{s}'\n", .{trimmed});
 
         return trimmed;
     }
