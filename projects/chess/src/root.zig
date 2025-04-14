fcbccb124f510533389ca2716cc96e6e5de54e8b
Jeffrey Meyer <jmeyer@swankmp.com>
2025-04-13 19:56:14 -0500

    move direction.zig to new chess project

--- projects/chess/src/root.zig
+++ projects/chess/src/root.zig
@@ -4,6 +4,8 @@ const std = @import("std");
 
 /// The player module contains types and functions related to players in chess.
 pub const player = @import("./player.zig");
+/// The direction module contains types and functions related directionality on a chess board.
+pub const direction = @import("./direction.zig");
 
 test {
     std.testing.refAllDeclsRecursive(@This());
