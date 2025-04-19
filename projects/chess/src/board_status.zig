0278edcd3c8ab7585a414575d94a7863c38a200f
Jeffrey Meyer <jmeyer@swankmp.com>
2025-04-19 12:03:27 -0500

    Add methods to board_status.zig

--- projects/chess/src/board_status.zig
+++ projects/chess/src/board_status.zig
@@ -2,6 +2,7 @@
 const std = @import("std");
 const Player = @import("./player.zig").Player;
 const CastleAbilities = @import("./castle.zig").CastleAbilities;
+const CastleDirection = @import("./castle.zig").CastleDirection;
 const File = @import("./square.zig").File;
 
 /// Represents an optional file. This is used for the en passant square, which can be null if there is no en passant square.
@@ -42,10 +43,66 @@ pub const OptionalFile = packed struct {
 
 /// Represents the status of the current board (used for comptime specific board functions).
 pub const BoardStatus = packed struct {
+    const Self = @This();
     /// The player who is currently to move.
     side_to_move: Player,
     /// The castling abilities of the current position.
     castle_abilities: CastleAbilities,
     /// The en passant square, if any.
     en_passant_file: OptionalFile,
+
+    pub fn init(
+        side_to_move: Player,
+        castle_abilities: CastleAbilities,
+        en_passant_file: OptionalFile,
+    ) BoardStatus {
+        return Self{
+            .side_to_move = side_to_move,
+            .castle_abilities = castle_abilities,
+            .en_passant_file = en_passant_file,
+        };
+    }
+
+    pub fn kingMove(self: Self) Self {
+        return Self{
+            .side_to_move = self.side_to_move.opposite(),
+            // TODO: Consider checking for existing rights before removing or debug assert
+            .castle_abilities = self.castle_abilities.kingMove(self.side_to_move),
+            .en_passant_file = OptionalFile.initNull(),
+        };
+    }
+
+    pub fn rookMove(self: Self, castle_direction: CastleDirection) Self {
+        return Self{
+            .side_to_move = self.side_to_move.opposite(),
+            // TODO: Consider checking for existing rights before removing or debug assert
+            .castle_abilities = self.castle_abilities.rookMove(self.side_to_move, castle_direction),
+            .en_passant_file = OptionalFile.initNull(),
+        };
+    }
+
+    pub fn rookCapture(self: Self, castle_direction: CastleDirection) Self {
+        return Self{
+            .side_to_move = self.side_to_move.opposite(),
+            // TODO: Consider checking for existing rights before removing or debug assert
+            .castle_abilities = self.castle_abilities.rookMove(self.side_to_move.opposite(), castle_direction),
+            .en_passant_file = OptionalFile.initNull(),
+        };
+    }
+
+    pub fn doublePawnMove(self: Self, file: File) Self {
+        return Self{
+            .side_to_move = self.side_to_move.opposite(),
+            .castle_abilities = self.castle_abilities,
+            .en_passant_file = OptionalFile.init(file),
+        };
+    }
+
+    pub fn quietMove(self: Self) Self {
+        return Self{
+            .side_to_move = self.side_to_move.opposite(),
+            .castle_abilities = self.castle_abilities,
+            .en_passant_file = OptionalFile.initNull(),
+        };
+    }
 };
