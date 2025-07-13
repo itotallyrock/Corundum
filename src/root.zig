84959ae3d2ca0f84593fbbfff3b0a3122411ddab
Jeffrey Meyer <jam93t@mst.edu>
2025-07-13 13:36:39 -0500

    setup root library and main differently

--- src/root.zig
+++ src/root.zig
@@ -1,13 +1,66 @@
-//! By convention, root.zig is the root source file when making a library. If
-//! you are making an executable, the convention is to delete this file and
-//! start with main.zig instead.
 const std = @import("std");
-const testing = std.testing;
 
-pub export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+/// The potential states of the UCI engine
+pub const UciEngineState = union(enum) {
+    /// The engine is not initialized (we have not received the "isready" command)
+    uninitialized: struct {},
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
-}
+    /// We've sent the "isready" command but aren't searching yet
+    ready: struct {},
+
+    /// The engine is searching
+    thinking: struct {},
+};
+
+const maximum_command_length = 4096;
+
+const logger = std.log.scoped(.engine_manager);
+
+/// The manager for the UCI engine
+pub const UciEngineManager = struct {
+    const Self = @This();
+    /// The input stream for the engine
+    input_stream: std.io.AnyReader,
+    /// The output stream for the engine
+    output_stream: std.io.AnyWriter,
+    /// Root allocator for the engine, used to allocate memory inside of child searchers
+    allocator: std.mem.Allocator,
+    /// The current state of the engine
+    state: UciEngineState = .uninitialized,
+
+    pub fn init(input_stream: std.io.AnyReader, output_stream: std.io.AnyWriter, allocator: std.mem.Allocator) Self {
+        return .{
+            .input_stream = input_stream,
+            .output_stream = output_stream,
+            .allocator = allocator,
+        };
+    }
+
+    pub fn run(self: *Self) !void {
+        var buffer: [maximum_command_length]u8 = undefined;
+        var buffer_stream = std.io.fixedBufferStream(&buffer);
+        while (true) {
+            self.input_stream.streamUntilDelimiter(buffer_stream.writer(), '\n', maximum_command_length) catch |err| switch (err) {
+                error.StreamTooLong => {
+                    logger.err("command too long", .{});
+                    continue;
+                },
+                error.EndOfStream => {
+                    logger.warn("input stream closed", .{});
+                    return;
+                },
+                else => return err,
+            };
+
+            const command = buffer_stream.getWritten();
+            defer buffer_stream.reset();
+            logger.debug("received command: {s}", .{command});
+
+            const parsed_command = std.mem.trim(u8, command, " \r\n\t");
+            if (std.ascii.startsWithIgnoreCase(parsed_command, "quit")) break;
+            // TODO: parse the command
+            // UciCommand.parse(parsed_command) catch |err| switch (err) {}
+            // TODO: execute the command based on the current state and the parsed command
+        }
+    }
+};
