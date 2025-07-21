5c4b4696e209712d43e9a705649b7c14b182d3c6
Jeffrey Meyer <jam93t@mst.edu>
2025-07-20 21:08:25 -0500

    add check method for exiting search when max nodes is reached to node_counter.zig

--- projects/search/src/node_counter.zig
+++ projects/search/src/node_counter.zig
@@ -6,6 +6,11 @@ pub const max_nodes = @import("search_build_options").max_nodes;
 /// A ply is a half-move in chess. It is used to represent the number of moves made in a game.
 pub const NodeCount = std.math.IntFittingRange(0, max_nodes);
 
+pub const NodeCounterError = error{
+    /// The maximum number of nodes has been reached
+    MaxNodesReached,
+};
+
 /// A NodeCounter is used to track the number of nodes a search has reached.
 pub const NodeCounter = struct {
     count: std.atomic.Value(NodeCount) = .init(0),
@@ -21,6 +26,21 @@ pub const NodeCounter = struct {
         try std.testing.expectEqual(counter.count.load(.seq_cst), 1);
         counter.increment();
         try std.testing.expectEqual(counter.count.load(.seq_cst), 2);
+    }
+
+    /// Checks if the maximum number of nodes has been reached
+    pub fn check(self: *const NodeCounter, max_search_nodes: NodeCount) NodeCounterError!void {
+        if (self.count.load(.seq_cst) >= max_search_nodes) {
+            return error.MaxNodesReached;
+        }
+    }
+
+    test check {
+        var counter = NodeCounter{};
+        counter.increment();
+        try counter.check(2);
+        counter.increment();
+        try std.testing.expectError(NodeCounterError.MaxNodesReached, counter.check(2));
     }
 };
 
