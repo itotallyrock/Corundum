1a211f84094f5c7e788ad95cc5cecef59478778d
Jeffrey Meyer <jam93t@mst.edu>
2025-07-22 20:11:38 -0500

    implement simple uci engine commands id, readyok, uciok

--- projects/uci/src/root.zig
+++ projects/uci/src/root.zig
@@ -3,6 +3,9 @@
 const std = @import("std");
 
 pub const AnyGuiCommand = @import("./gui_commands/any.zig").AnyGuiCommand;
+pub const UciOk = @import("./engine_commands/uci_ok.zig").UciOk;
+pub const Id = @import("./engine_commands/id.zig").Id;
+pub const ReadyOk = @import("./engine_commands/ready_ok.zig").ReadyOk;
 
 test {
     std.testing.refAllDeclsRecursive(@This());
