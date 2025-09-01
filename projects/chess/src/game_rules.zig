4384d06e55fb728e891aa64ddaea70b49a7c8326
Jeffrey Meyer itotallyrock <itotallyrock@users.noreply.github.com>
2025-09-01 06:31:08 -0500

    remove board dimensions for now until it can be thought out more

--- projects/chess/src/game_rules.zig
+++ projects/chess/src/game_rules.zig
@@ -23,44 +23,13 @@ pub const ThreefoldRepetition = union(BooleanRule) {
     disabled: struct {},
 };
 
-/// How big the board should be
-pub const BoardDimensions = struct {
-    const Self = @This();
-
-    /// The number of rows on the board
-    ranks: u16,
-    /// The number of columns on the board
-    files: u16,
-
-    /// If the height and width are the same
-    pub fn isSquare(self: Self) bool {
-        return self.ranks == self.files;
-    }
-
-    test isSquare {
-        try std.testing.expectEqual((BoardDimensions{ .ranks = 8, .files = 8 }).isSquare(), true);
-        try std.testing.expectEqual((BoardDimensions{ .ranks = 32, .files = 32 }).isSquare(), true);
-        try std.testing.expectEqual((BoardDimensions{ .ranks = 2, .files = 2 }).isSquare(), true);
-        try std.testing.expectEqual((BoardDimensions{ .ranks = 8, .files = 1 }).isSquare(), false);
-        try std.testing.expectEqual((BoardDimensions{ .ranks = 1, .files = 8 }).isSquare(), false);
-        try std.testing.expectEqual((BoardDimensions{ .ranks = 8, .files = 18 }).isSquare(), false);
-    }
-};
-
-pub const @"8x8": BoardDimensions = .{
-    .ranks = 8,
-    .files = 8,
-};
-
 /// The game rules to tell the engine_commands how to play the game.
 pub const GameRules = struct {
     const Self = @This();
     /// The standard game rules for chess.
-    pub const standard = Self.init(@"8x8", .standard, .enabled, .enabled, .enabled);
-    pub const fischer = Self.init(@"8x8", .fischer_random, .enabled, .enabled, .enabled);
+    pub const standard = Self.init(.standard, .enabled, .enabled, .enabled);
+    pub const fischer = Self.init(.fischer_random, .enabled, .enabled, .enabled);
 
-    /// The size of the board
-    board_dimensions: BoardDimensions,
     /// Configure how castle rules are applied.
     castle_game_type: CastleGameType,
     /// Whether the fifty-move rule is enabled.
@@ -72,14 +41,12 @@ pub const GameRules = struct {
 
     /// Create a new set of game rules.
     pub fn init(
-        board_dimensions: BoardDimensions,
         castle_game_type: CastleGameType,
         fifty_move_limit: BooleanRule,
         threefold_repetition: BooleanRule,
         insufficient_material: BooleanRule,
     ) Self {
         return Self{
-            .board_dimensions = board_dimensions,
             .castle_game_type = castle_game_type,
             .fifty_move_limit = fifty_move_limit,
             // if threefold_repetition is enabled, set the history size to 100 if fifty_move_limit is enabled, otherwise set it to max_plies
